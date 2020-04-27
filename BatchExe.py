568e8fe286ffbbba6360b3616e87378be1351feb
Enrico Rossini <enricoross98@icloud.com>
2020-04-27 15:52:37 +0200

    Aggiusto valori simulazione

--- BatchExe.py
+++ BatchExe.py
@@ -14,13 +14,13 @@ t_coda = open("Output/t_in_coda.txt", "w")  # scrivo il tempo medio in coda risp
 # -------------------- VARIABILI MODIFICABILI -------------------- #
 
 from_auto_test = 10  # (per simulazione impostare a 10)
-to_auto_test = 50  # (per simulazione impostare a 100 o 200 se abbasatanza efficente)
+to_auto_test = 200  # (per simulazione impostare a 100 o 200 se abbasatanza efficente)
 step_auto_test = 10  # (per simulazione impostare a 10)
-prove_fissate_auto = 2  # (per simulazione impostare a 10)
+prove_fissate_auto = 10  # (per simulazione impostare a 10)
 max_auto_insieme = 12  # solo per Version4 e Versione7
 gui = False
 n_porta_base = 5000
-prove_una_auto = 1  # (per simulazione impostare a 20)
+prove_una_auto = 20  # (per simulazione impostare a 20)
 
 # ---------------------------------------------------------------- #
 #
